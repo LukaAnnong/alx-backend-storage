6146a8bd0f485dc26bccb7a271128c5532bc2523
LukaAnnong <Annongluka@yahoo.com>
2023-10-19 10:54:29 +0000

    backend

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
-
-"""
-Return the List of Schools with a Specific Topic
-"""
+""" Module for using PyMongo """
 
 
 def schools_by_topic(mongo_collection, topic):
-    """
-    Return the list of schools that have a specific topic
-
-    Args:
-        mongo_collection: The pymongo collection object
-        topic (str): The topic to search for
-
-    Returns:
-        A list of schools with the specified topic
-    """
-    return mongo_collection.find({"topics": topic}
+    """ Returns the list of school having a specific topic """
+    return mongo_collection.find({"topics": topic})
